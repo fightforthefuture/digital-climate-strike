a8e6f5bb45b9652dd1687f22db758e5d667807e3
fviejobueno <francisco.v@thoughtworks.com>
2019-08-16 17:25:30 +0200

    [Add German language support #14] + Included handlebars dependency in package.json configured handlebars plugin in webpack-common included static resources folder for yml translations included handlebars placeholders in index.html
    -
    Testing missing

--- webpack.common.js
+++ webpack.common.js
@@ -4,6 +4,30 @@ const CopyPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const path = require('path')
+const handlebars = require('handlebars')
+
+function loadStrings(languageCode) {
+  const stringsFile = path.resolve(__dirname, 'src', 'translations', `${languageCode}.yml`)
+  const strings = yaml.safeLoad(fs.readFileSync(stringsFile, 'utf8'))
+  return formatStrings(strings)
+}
+
+function HandlebarsPlugin(options) {
+  options = options || {};
+  this.template = options.template;
+}
+
+HandlebarsPlugin.prototype.apply = function(compiler) {
+  compiler.hooks.compilation.tap('HandlebarsPlugin', function (compilation) {
+    compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync('HandlebarsPlugin', function (data, callback) {
+      const template = handlebars.compile(data.html)
+      const strings = loadStrings(data.plugin.options.language)
+      data.html = template(strings)
+      callback(null, data)
+    });
+  });
+}
+
 
 module.exports = {
   entry: './src/index.js',
@@ -70,6 +94,7 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
+    new HandlebarsPlugin(),
     new HtmlWebPackPlugin({
       template: './src/index.html',
       inlineSource: '.(js|css)$',
