e70e15178cf3440ae1ce2fa0e33317c79472963b
Sally Northmore <snorthmore@brilliantbasics.com>
2019-08-17 12:06:53 +0100

    Update footer styles to match design

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,13 @@ module.exports = {
   plugins: {
     'postcss-import': {},
     'postcss-preset-env': {
+      /* support last 2 versions of all browsers */
       browsers: 'last 2 versions',
+      /* use stage 0 features + css nesting rules */
+      stage: 0,
+      features: {
+        'nesting-rules': true
+      },
     },
     'cssnano': {},
   },
