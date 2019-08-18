55ad107e526657c9f14bbda91fcb489af4c78953
Sally Northmore <snorthmore@brilliantbasics.com>
2019-08-18 20:25:06 +0100

    Revert js file

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ function initializeInterface() {
 
   attachEvent('.dcs-close', 'click', closeWindow)
 
-  const isDayOfAction = todayIs(2019, 8, 28)
+  const isDayOfAction = todayIs(2019, 9, 20)
 
   if (query.fullPage || isDayOfAction) {
     maximize()
