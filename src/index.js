4259bed25a11d7ca4c9434b84fbf6733c609e8cd
sturosier <sturosier0691@gmail.com>
2019-09-04 10:10:46 -0400

    Allow the option to use different urls for the footer and full-page of the widget

--- src/index.js
+++ src/index.js
@@ -120,7 +120,7 @@ function attachEvent(selector, event, callback) {
 function initGoogleAnalytics() {
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga')
 
   if (typeof window.ga !== 'undefined') {
